8b0be623d568e6d223dfaa110d4933ba06747818
Andreas Falk <andreas.falk@novatec-gmbh.de>
2016-03-23 11:02:30 +0100

    added gitignore and saml sample client

---
+++ saml-client/src/main/java/info/novatec/SamlClientApplication.java
@@ -0,0 +1,12 @@
+package info.novatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlClientApplication.class, args);
+	}
+}
