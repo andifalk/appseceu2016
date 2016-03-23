cd9d47f42d4e01890717374ffcd21d6c8a60cffd
Andreas Falk <andreas.falk@novatec-gmbh.de>
2016-03-23 21:35:31 +0100

    simplified dependencies

--- oauth-client/src/main/java/info/novatec/oauth/client/OAuthClientApplication.java
+++ oauth-client/src/main/java/info/novatec/oauth/client/OAuthClientApplication.java
@@ -2,11 +2,13 @@ package info.novatec.oauth.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
+@EnableOAuth2Sso
 @SpringBootApplication
-public class OauthClientApplication {
+public class OAuthClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OauthClientApplication.class, args);
+		SpringApplication.run( OAuthClientApplication.class, args);
 	}
 }
